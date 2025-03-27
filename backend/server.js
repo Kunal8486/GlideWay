5292be8db1d68c4a9182ab9dc79373fa92d48aa8
Kunal8486 <Kunalkumar8486@gmail.com>
2025-03-27 05:43:16 +0530

    modified:   backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: process.env.FRONTEND_URL,
   credentials: true, 
   allowedHeaders: ["Content-Type", "Authorization"]
 }))
